2d18f0a4d29eb9ccb3043f2f47bb4079ce8ffeb8
Nicholas Jacob <nicholas@Nicholass-Mac-mini.local>
2019-10-15 15:27:51 -0700

    added swift files for breed data and set up structs and data

---
+++ DogBreeds/BreedData.swift
@@ -0,0 +1,9 @@
+//
+//  BreedData.swift
+//  DogBreeds
+//
+//  Created by Nicholas Jacob on 10/15/19.
+//  Copyright © 2019 Nicholas Jacob. All rights reserved.
+//
+
+import Foundation
---
+++ DogBreeds/LabelsAndIconsView.swift
@@ -0,0 +1,21 @@
+//
+//  LabelsAndIconsView.swift
+//  DogBreeds
+//
+//  Created by Nicholas Jacob on 10/15/19.
+//  Copyright © 2019 Nicholas Jacob. All rights reserved.
+//
+
+import SwiftUI
+
+struct LabelsAndIconsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LabelsAndIconsView_Previews: PreviewProvider {
+    static var previews: some View {
+        LabelsAndIconsView()
+    }
+}
